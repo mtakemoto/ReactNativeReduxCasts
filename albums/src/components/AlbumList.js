565778c8b447d37679a77c7d91e99d677e0a306e
Matthew Takemoto <matt.t.takemoto@gmail.com>
2017-04-09 13:39:54 -0700

    implementing albumlist

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 /* This is a functional component
  * Must return some kind of JSX
  * Babel requires the base React class
+ *
  */
 class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    //Set state to have array of objects on recieving request
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    //Want unique value for key consistent across renders.  Idx isn't consistent
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+
   render() {
+    console.log(this.state);
+
     return (
       <View>
-        <Text>Album List!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
